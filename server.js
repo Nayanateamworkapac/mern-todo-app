ee7203f7676df6cdf9e6463339510488f5605154
NAYANA <nayana@teamworkapac.com>
2025-11-22 18:45:48 +0530

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const tasksRouter = require('./routes/tasks');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use('/tasks', tasksRouter);
